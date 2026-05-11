37d619b34dabc4a984ca91e4cf9db01d6212b922
Tien Nguyen <tnguyen@profoundlogic.com>
2026-05-11 11:02:56 -0700

    add data used by COBOL program `inq01l.cblle`

---
+++ data/categp.sql
@@ -0,0 +1,17 @@
+truncate table categp;
+insert into categp values
+(101, 'Navigation Systems            '),
+(103, 'Update Plans                  '),
+(104, 'Mobile Phones                 '),
+(105, 'Smart Phone                   '),
+(106, 'Bluetooth Headsets            '),
+(107, 'Monitors                      '),
+(108, 'Laptops                       '),
+(109, 'Computers                     '),
+(110, 'Printers                      '),
+(111, 'Scanners                      '),
+(112, 'TV''s                          '),
+(113, 'Digital Cameras               '),
+(114, 'MP3 Players                   '),
+(115, 'Projectors                    '),
+(102, 'Accessories                   ');
---
+++ data/featuresp.sql
@@ -0,0 +1,19 @@
+truncate table featuresp;
+insert into featuresp values
+(101, 'Custom POIs                   ', '                                                            '),
+(102, 'Photo Navigation              ', '                                                            '),
+(103, 'Traffic Updates               ', '                                                            '),
+(104, 'Traffic Route                 ', '                                                            '),
+(105, 'Eco Route                     ', '                                                            '),
+(106, 'Personalized Route            ', '                                                            '),
+(107, 'Route Choice                  ', '                                                            '),
+(108, 'Automatic Re-route            ', '                                                            '),
+(109, 'Voice Prompts                 ', '                                                            '),
+(110, 'Voice Recognition             ', '                                                            '),
+(111, 'Lane Assist                   ', '                                                            '),
+(112, 'Bluetooth                     ', '                                                            '),
+(113, 'Speed Limit                   ', '                                                            '),
+(114, 'Picture Viewer                ', '                                                            '),
+(115, 'Pinch                         ', '                                                            '),
+(116, 'Zoom                          ', '                                                            '),
+(117, '3D View                       ', '                                                            ');
